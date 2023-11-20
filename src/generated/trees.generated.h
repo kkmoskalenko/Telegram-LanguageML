8d837754e73b7b7db3097ece8bc8cf186ff9f1a5
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2023-11-20 22:37:31 +0700

    Add the generated sources

---
+++ src/generated/trees.generated.h
@@ -0,0 +1,10 @@
+#ifndef TGLANG_TREES_GENERATED_H
+#define TGLANG_TREES_GENERATED_H
+
+#include "../tglang.h"
+
+#define N_TREES 50
+
+extern enum TglangLanguage (*trees[N_TREES])(const double *);
+
+#endif //TGLANG_TREES_GENERATED_H
